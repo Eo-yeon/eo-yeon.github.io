6184e63976afe1fff98978e272d4079adf4925e1
Eo-yeon <Eo-yeon@users.noreply.github.com>
2025-08-09 14:30:44 +0000

    deploy: a2f80ced265fb947ee8d46ad245ba4646232dfe6

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754749131',resources: [
+    cacheName: 'chirpy-1754749843',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
