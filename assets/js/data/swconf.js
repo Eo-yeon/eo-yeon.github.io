afed1360b8f905f494e02bf44cbf329a70203781
Eo-yeon <Eo-yeon@users.noreply.github.com>
2025-08-09 13:57:41 +0000

    deploy: 50944536a12334fdd97554dc30cdf4e11afa75fc

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1754747860',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://eoyeon.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
